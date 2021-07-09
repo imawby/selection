a45ef5fb49870687cb4ebdc17b75cbd183c0b862
Isobel Mawby <imawby@dunegpvm02.fnal.gov>
2021-07-09 04:33:29 -0500

    job submission files

---
+++ jobSubmission/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export FHICL_FILE_PATH=/dune/app/users/imawby/selection/fcl:/dune/app/users/imawby/selection/fcl/MCC11:/dune/app/users/imawby/selection/fcl/MCC11/nu/cheatSelection/dune/app/users/imawby/selection/fcl/MCC11/anu/cheatSelection/dune/app/users/imawby/selection/fcl/MCC11/nu/cheatCharacterisation:/dune/app/users/imawby/selection/fcl/MCC11/anu/cheatCharacterisation:/dune/app/users/imawby/selection/fcl/MCC11/nu/cheatPandora:/dune/app/users/imawby/selection/fcl/MCC11/anu/cheatPandora:$FHICL_FILE_PATH
